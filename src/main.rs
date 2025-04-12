666496bce9a31ea2865633471949709043546dc0
Thomas Gebert <thomas@gebert.app>
2025-04-12 01:11:51 -0400

    Add proper async support and persistence

--- src/main.rs
+++ src/main.rs
@@ -1,328 +1,182 @@
-use std::{collections::HashMap, fs::read_to_string};
+use tokio::sync::mpsc::{channel, Receiver};
+use tokio::task::JoinHandle;
+use std::sync::{Arc, Mutex};
+use std::{collections::HashMap, fs::read_to_string, fs::write};
 use std::error::Error;
+mod types;
 use std::result::Result as StdResult;
-use std::time::Duration;
-use tokio::process::Command;
-use async_trait::async_trait;
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
 use futures::future::join_all;
-use chrono::{Datelike, Local, Timelike};
-use serde::{Deserialize, Serialize};
-use std::time::{SystemTime, UNIX_EPOCH};
+use types::Meta;
 
-
-fn bat_status_icons(n: &str) -> &'static str {
-   match n {
-       "full" => "🟢",
-       "charging" => "⚡",
-       "notcharging" => "🔌",
-       "discharging" => "🔋",
-       _ => ""
-   }
-}
-
-fn wifi_status_icons(n : &str) -> &'static str {
-
-    match n {
-        "connected" => "📶",
-        "disconnected" => "❌",
-        _ => ""
-    }
-}
-
-fn month_abbr(n: u32) -> &'static str {
-    match n {
-        1 => "Jan",
-        2 => "Feb",
-        3 => "Mar",
-        4 => "Apr",
-        5 => "May",
-        6 => "Jun",
-        7 => "Jul",
-        8 => "Aug",
-        9 => "Sep",
-        10 => "Oct",
-        11 => "Nov",
-        12 => "Dec",
-        _ => "???", // or "" or panic!("bad month")
-    }
-}
-
-struct Meta {
-   is_processing: bool,
-   start_time: Duration,
-   data: HashMap<String,String>
-}
-
-#[async_trait]
-trait Handler {
-    async fn handle(&self) -> StdResult<HashMap<String,String>, Box<dyn Error>>;
-    fn render(&self, i: HashMap<String,String>)-> String; 
-}
-
-struct Noop;
-
-#[async_trait]
-impl Handler for Noop {
-    async fn handle(&self) -> StdResult<HashMap<String,String>, Box<dyn Error>> {
-        let mut out_hash = HashMap::new();
-        out_hash.insert("".to_string(),"".to_string());
-        Ok(out_hash)
-    }
-    fn render(&self, i : HashMap<String, String>) -> String {
-        "".to_string()
-    }
-}
-
-struct Battery;
-
-#[async_trait]
-impl Handler for Battery {
-    async fn handle(&self) -> StdResult<HashMap<String,String>, Box<dyn Error>> {
-        let bat_path = "/sys/class/power_supply/BAT0";
-        let cap_path = format!("{}/capacity", bat_path);
-        let stat_path = format!("{}/status", bat_path);
-        let cap_string = std::fs::read_to_string(cap_path)?.trim().replace("\"", "");
-        let stat_string = std::fs::read_to_string(stat_path)?.trim().to_lowercase().replace(" ", "");
-
-        let mut out_map = HashMap::new(); 
-        out_map.insert("capacity".to_string(), cap_string.to_string());
-        out_map.insert("status".to_string(), stat_string.to_string());
-        Ok(out_map) 
-    }
-
-    fn render(&self, i : HashMap<String, String>) -> String {
-        let empty = "".to_string();
-        let cap = i.get("capacity").unwrap_or(&empty);
-        let stat = i.get("status").unwrap_or(&empty).as_str();
-        format!("{} {}%", bat_status_icons(stat) , cap)
-    }
-}
-
-struct Wifi;
-
-#[async_trait]
-impl Handler for Wifi {
-    async fn handle(&self) -> StdResult<HashMap<String,String>, Box<dyn Error>> {
-
-        let wifi_cmd = Command::new("iw").arg("dev").output().await?;
-        let s : Vec<String> = String::from_utf8_lossy(&wifi_cmd.stdout).lines().map(|s| s.trim().to_string()).collect();
-        let interface : &str = s[5].split(" ").last().unwrap_or("");
-
-        let connected_cmd = Command::new("iw").arg(interface).arg("link").output().await?;
-        let s2 = String::from_utf8_lossy(&connected_cmd.stdout).find("Connected");
-
-        let is_connected = match s2 {
-            Some(_) => true,
-            None => false 
-        };
-
-        let connect_status = if is_connected {"connected"} else {"disconnected"};
-
-        let out_map : HashMap<String, String>  = 
-            [("connect_status", connect_status)]
-            .iter()
-                .map(|(k,v)| (k.to_string(), v.to_string()))
-                .collect();
-        //out_map.insert("connect_status".to_string(), connect_status.to_string());
-
-        Ok(out_map)
-    }
-
-    fn render(&self, i : HashMap<String, String>) -> String {
-
-        let EMPTY = "".to_string();
-        let connected = i.get("connect_status").unwrap_or(&EMPTY);
-        format!("{}", wifi_status_icons(&connected))
-    }
-}
-
-struct Volume;
-#[async_trait]
-impl Handler for Volume {
-    async fn handle(&self) -> StdResult<HashMap<String,String>, Box<dyn Error>> {
-        let SPACE = " ".to_string();
-        let is_muted_cmd = Command::new("pactl").arg("get-sink-mute").arg("@DEFAULT_SINK@").output().await?;
-        let vol_info_cmd = Command::new("pactl").arg("get-sink-volume").arg("@DEFAULT_SINK@").output().await?; 
-        let vol_info_str = String::from_utf8_lossy(&vol_info_cmd.stdout);
-        let vol_info : Vec<String> = vol_info_str.split(&SPACE).map(|i| i.to_string()).collect();
-        let is_muted_raw_str = String::from_utf8_lossy(&is_muted_cmd.stdout);
-        let is_muted_lower = is_muted_raw_str.to_lowercase();
-        let is_muted_str = is_muted_lower.split(&SPACE).map(|x| x.trim()).last(); 
-
-        let is_muted = match is_muted_str {
-            Some("yes") => "muted",
-            _ => "not muted" 
-        }.to_string();
-
-        let vol_level = if vol_info[5] == "/" {vol_info[4].to_string()} else {vol_info[5].to_string()}.replace("%","");
-        let out_map : HashMap<String, String> = [("volume_level", vol_level), ("is_muted", is_muted)].iter().map(|(k,v)| (k.to_string(), v.to_string())).collect(); 
-
-        Ok(out_map)
-    }
-
-    fn render(&self, i : HashMap<String, String>) -> String {
-        let default_muted = "default_muted".to_string();
-        let is_muted = i.get("is_muted").unwrap_or(&default_muted);
-        let small_speaker_cutoff = 40;
-        let mid_speaker_cutoff = 80; 
-        let default_vol = "50".to_string();
-        let vol_level_str = i.get("volume_level").unwrap_or(&default_vol); 
-
-        let vol_level : i32 = vol_level_str.parse().unwrap_or(50); 
-
-        let icon = if is_muted == "muted" {
-            "🔇"
-        } else if vol_level < small_speaker_cutoff {
-            "🔈"
-        } else if vol_level < mid_speaker_cutoff {
-            "🔉"
-        } else {
-            "🔊"
-        };
-
-        format!("{}{}%", icon, vol_level_str)
-
-    }
-}
-
-
-struct Date;
-
-#[async_trait]
-impl Handler for Date{
-    async fn handle(&self) -> StdResult<HashMap<String,String>, Box<dyn Error>> {
-        let now = Local::now();
-        let weekday = now.weekday();
-        let day = now.day();
-        let month = month_abbr(now.month());
-        let seconds = format!("{:02}", now.second());
-        let mod_hour = if now.hour() % 12 == 0 {12} else {now.hour() % 12};
- 
-        let hour = format!("{:02}", mod_hour);  
-        let minutes = format!("{:02}", now.minute());
-        let mut out_hash = HashMap::new();
-        out_hash.insert("weekday".to_string(), weekday.to_string());
-        out_hash.insert("day".to_string(), day.to_string());
-        out_hash.insert("month".to_string(), month.to_string());
-        out_hash.insert("seconds".to_string(), seconds.to_string());
-        out_hash.insert("minutes".to_string(), minutes.to_string());
-        out_hash.insert("hour".to_string(), hour.to_string());
-        Ok(out_hash)
-    }
-
-    fn render(&self, i : HashMap<String, String>) -> String {
-        static EMPTY: String = String::new();
-
-        let hour = i.get("hour").unwrap_or(&EMPTY);
-        let minute = i.get("minutes").unwrap_or(&EMPTY);
-        let day = i.get("day").unwrap_or(&EMPTY);
-        let month = i.get("month").unwrap_or(&EMPTY);
-        let weekday = i.get("weekday").unwrap_or(&EMPTY);
-        let seconds = i.get("seconds").unwrap_or(&EMPTY);
-        format!("{} {} {} {}:{} {}", weekday, month, day, hour, minute, seconds )
-    }
-}
-
-fn get_handler(my_type: &str) -> Box<dyn Handler> {
+fn get_handler(my_type: &str) -> Box<dyn types::Handler> {
     match my_type {
-        "date" => Box::new(Date),
-        "battery" => Box::new(Battery),
-        "wifi" => Box::new(Wifi),
-        "volume" => Box::new(Volume),
-        _ => Box::new(Noop)
+        "date" => Box::new(types::Date),
+        "battery" => Box::new(types::Battery),
+        "wifi" => Box::new(types::Wifi),
+        "volume" => Box::new(types::Volume),
+        "quote" => Box::new(types::Quote),
+        _ => Box::new(types::Noop)
     }
 }
 
-
-#[derive(Serialize, Deserialize)]
-struct Out {
-   name : String, 
-   instance: String,
-   full_text: String
-}
-
-#[derive(Serialize, Deserialize)]
-struct PersistConfig  {
-    path: String,
-    buffer_size : i32
-}
-
-#[derive(Serialize, Deserialize)]
-struct ModuleConfig {
-   name: String,
-   ttl: u64
-}
-
-#[derive(Serialize, Deserialize)]
-struct Config {
-   poll_time : i32,
-   default_timeout: i32, 
-   persist : PersistConfig, 
-   modules : Vec<ModuleConfig>
-}
-
-#[tokio::main]
-async fn main() -> StdResult<(), Box<dyn Error>> {
-    let poll_time_ms = Duration::from_millis(50);
-    //let handlers = [ "wifi", "battery", "volume", "date"].map(|i| (i, get_handler(i)));
-    let mut state : HashMap<String, Meta>= HashMap::new(); 
-    let config_str = read_to_string("swaybar-config.json")?;
-    let config : Config =  serde_json::from_str(config_str.as_str())?;
-
+async fn render(mut chan : Receiver<Vec<types::Out>>) {
     println!("{}","{\"version\":1, \"click_events\":true}");
     println!("[");
     println!("[],");
+    tokio::task::spawn(async move {
+        while let Some(msg) = chan.recv().await {
+            let out_json = serde_json::to_string(&msg);
+            match out_json {
+                Ok(out) => println!("{}", out),
+                Err(_) => ()
+            }
+        }
+    });
+}
+
+async fn write_state(mut chan: Receiver<HashMap<String,Meta>>) {
+    tokio::task::spawn(async move {
+        let mut counter = 0; 
+        while let Some(msg) = chan.recv().await {
+            if counter % 100 == 0 {
+                let out_json = serde_json::to_string(&msg);
+                match out_json {
+                    Ok(x) => {write("state.json", x);},
+                    Err(_) => ()
+                };
+            }
+            counter+=1; 
+        }
+
+    });
+
+}
+
+
+#[tokio::main(flavor = "multi_thread", worker_threads = 1)]
+async fn main() -> StdResult<(), Box<dyn Error>> {
+    let config_str = read_to_string("swaybar-config.json")?;
+    let config : types::Config =  serde_json::from_str(config_str.as_str())?;
+    let init_state_str = match read_to_string("state.json") {
+        Ok(my_str) => my_str,
+        Err(_) => "{}".to_string()
+    };
+    let mut state : HashMap<String, Meta> = serde_json::from_str(init_state_str.as_str())?;
+
+    let poll_time_ms = Duration::from_millis(config.poll_time);
+    //let mut state : HashMap<String, types::Meta>= HashMap::new();
+    let (render_sender, render_receiver) = tokio::sync::mpsc::channel::<Vec<types::Out>>(10);
+    render(render_receiver).await;
+
+    let (state_sender, state_receiver) = tokio::sync::mpsc::channel::<HashMap<String,Meta>>(10);
+    write_state(state_receiver).await;
+
     loop {
         let loop_begin = std::time::Instant::now();
-        let loop_now = SystemTime::now().duration_since(UNIX_EPOCH).unwrap_or(Duration::ZERO); 
-        
-        let futs = config.modules.iter().map(|i| async {
-            let now = SystemTime::now(); 
-            let epoch = now.duration_since(UNIX_EPOCH).unwrap_or(Duration::ZERO);
-            let ttl = Duration::from_millis(i.ttl); 
-            let ttl_expire = now + ttl; 
-            let name = &i.name;
-            let handler = get_handler(i.name.as_str());
-            let fut = handler.handle();
-            let res = tokio::select! {
-                res = fut => res,
-                else => {
-                    let f = 
-                        Meta {is_processing: true, 
-                            start_time: epoch,
-                            data : HashMap::new()
-                        }; 
-                    Ok(state.get(&name.to_string()).unwrap_or(&f).data)
+        let futures = Arc::new(Mutex::new(HashMap::<String, JoinHandle<HashMap<String, String>>>::new()));
 
-                }
+        
+        let futs = config.modules.iter().map(|module_config| {
+            let default = Meta {
+                is_processing : false,
+                start_time : Duration::ZERO,
+                data: HashMap::new()
+
             };
-            match res {
-                Ok(hm) => (name.to_string(), hm.clone(), handler.render(hm)),
-                Err(_e) => (name.to_string(), HashMap::new(), "".to_string())
-            }
-        });
+            let begin_state = state.get(&module_config.name).unwrap_or(&default).clone();
+            let handler1 = get_handler(module_config.name.as_str());
+            let handler2 = get_handler(module_config.name.as_str());
+            let ttl = Duration::from_millis(module_config.ttl);
+            let name = module_config.name.clone();
+
+            let futures = futures.clone();
+            async move {
+                let now = SystemTime::now()
+                    .duration_since(UNIX_EPOCH)
+                    .expect("Time went backwards");
+                let expire_time = begin_state.start_time + ttl; 
+                let begin_data = begin_state.data.clone();
+                let  new_state =  if !begin_state.is_processing && now > expire_time {
+                    let fut = tokio::spawn(async move {
+                        let f = handler1.handle().await; 
+                        match f {
+                            Ok(ff) => ff,
+                            Err(_) => begin_data
+                        }
+                    });
+
+                    let ns = Meta {
+                        is_processing : true, 
+                        start_time : now, 
+                        data : begin_state.data.clone()
+                    };
+                    let mut lock = futures.lock().unwrap();
+                    lock.insert(name.clone(), fut);
+                    ns
+
+                } else {
+                    begin_state
+                };
+
+                let mut fut = {
+                    let mut lock = futures.lock().unwrap();
+                    if let Some(handle) = lock.remove(&name) {
+                        handle
+                    } else {
+                        tokio::spawn(async { 
+                            HashMap::new()
+                        })
+                    }
+                };
+
+                let (new_new_state, res) = match tokio::select! {
+                    res = &mut fut => Some(res),
+                    else => None,
+                } {
+                    Some(Ok(data)) => {
+                        let mut temp = new_state.clone();
+                        temp.data.extend(data.clone());
+                        let new_meta = Meta {
+                            is_processing: false,
+                            start_time: new_state.start_time, 
+                            data: temp.data
+                        };
+                        (new_meta, data)
+                    },
+                    Some(Err(_)) => {
+                        let mut lock = futures.lock().unwrap();
+                        lock.insert(name.clone(), fut);
+
+                        (new_state.clone(), new_state.data.clone())
+                    },
+                    None => 
+                    {
+                        let mut lock = futures.lock().unwrap();
+                        lock.insert(name.clone(), fut);
+                        (new_state.clone(), new_state.data.clone())
+                    },
+                };
+
+
+                let nnsd = new_new_state.clone().data.clone();
+                (name.to_string(), new_new_state, handler2.render(nnsd))
+            }});
 
         let values = join_all(futs).await; 
 
-        let out_objs: Vec<Out> = values.iter().map(|(name, data, out_str)|{
-            let f = Meta {
-                start_time: loop_now,
-                data : HashMap::new(),
-                is_processing: false
-            };
-            let curr_state = state.entry(name.to_string()).or_insert_with(|| f);
-            data.iter().for_each(|(k, v)| {
-                curr_state.data.insert(k.to_string(), v.to_string());
-            });
-            Out {
+        let out_objs: Vec<types::Out> = values.into_iter().map(|(name, meta, out_str)|{
+            state.insert(name.clone(), meta.clone());
+            types::Out {
                 name: name.clone(),
                 instance: name.clone(),
                 full_text: out_str.to_string()
             }
         }).collect();
 
-        let out_json = serde_json::to_string(&out_objs)?;
-        println!("{},", out_json);
+        render_sender.send(out_objs).await?;
+        state_sender.send(state.clone()).await?;
+
         let elapsed = loop_begin.elapsed();
         let wait_time = poll_time_ms.checked_sub(elapsed).unwrap_or(Duration::ZERO);
         tokio::time::sleep(wait_time).await;
---
+++ src/types.rs
@@ -0,0 +1,340 @@
+
+use std::{collections::HashMap, fs::read_to_string};
+use chrono::{Datelike, Local, Timelike};
+use reqwest::Client;
+use serde::{Deserialize, Serialize};
+use tokio::process::Command;
+
+use std::error::Error;
+use std::result::Result as StdResult;
+use async_trait::async_trait;
+
+use std::time::Duration;
+
+
+#[derive(Clone, Serialize, Deserialize)]
+pub struct Meta {
+   pub is_processing: bool,
+   pub start_time: Duration,
+   pub data: HashMap<String,String>,
+       
+}
+
+fn wifi_status_icons(n : &str) -> &'static str {
+
+    match n {
+        "connected" => "📶",
+        "disconnected" => "❌",
+        _ => ""
+    }
+}
+
+
+fn month_abbr(n: u32) -> &'static str {
+    match n {
+        1 => "Jan",
+        2 => "Feb",
+        3 => "Mar",
+        4 => "Apr",
+        5 => "May",
+        6 => "Jun",
+        7 => "Jul",
+        8 => "Aug",
+        9 => "Sep",
+        10 => "Oct",
+        11 => "Nov",
+        12 => "Dec",
+        _ => "???", // or "" or panic!("bad month")
+    }
+}
+
+#[async_trait]
+pub trait Handler: Send + Sync {
+    async fn handle(&self) -> StdResult<HashMap<String,String>, Box<dyn Error >>;
+    fn render(&self, i: HashMap<String,String>)-> String; 
+}
+
+#[derive(Clone)]
+pub struct Noop;
+
+#[async_trait]
+impl Handler for Noop {
+    async fn handle(&self) -> StdResult<HashMap<String,String>, Box<dyn Error>> {
+        let mut out_hash = HashMap::new();
+        out_hash.insert("".to_string(),"".to_string());
+        Ok(out_hash)
+    }
+    fn render(&self, i : HashMap<String, String>) -> String {
+        "".to_string()
+    }
+}
+
+
+
+#[derive(Serialize)]
+struct Message {
+    role: String,
+    content: String,
+}
+
+#[derive(Serialize)]
+struct ChatRequest {
+    model: String,
+    messages: Vec<Message>,
+}
+
+#[derive(Deserialize)]
+struct Choice {
+    message: MessageContent,
+}
+
+#[derive(Deserialize)]
+struct MessageContent {
+    content: String,
+}
+
+#[derive(Deserialize)]
+struct ChatResponse {
+    choices: Vec<Choice>,
+}
+
+pub async fn get_inspirational_quote(api_key: &str, prompt: &str) -> Result<String, Box<dyn Error>> {
+    let client = Client::new();
+
+    let body = ChatRequest {
+        model: "gpt-3.5-turbo".to_string(),
+        messages: vec![Message {
+            role: "user".to_string(),
+            content: prompt.to_string(),
+        }],
+    };
+
+    let res = client
+        .post("https://api.openai.com/v1/chat/completions")
+        .bearer_auth(api_key)
+        .json(&body)
+        .send()
+        .await?;
+
+    let data: ChatResponse = res.json().await?;
+    let quote = &data.choices.get(0).unwrap().message.content;
+
+    Ok(quote.trim().to_string())
+}
+
+#[derive(Clone)]
+pub struct Quote;
+
+#[async_trait]
+impl Handler for Quote {
+    async fn handle(&self) -> StdResult<HashMap<String,String>, Box<dyn Error>> {
+
+        let config_str = read_to_string("/home/tombert/openai.key")?;
+        let config_str = config_str.trim();
+        let quote = get_inspirational_quote(config_str,  "Give me an inspirational quote about panda bears").await?;
+        let out_map: HashMap<String, String> = [("quote", quote)].iter().map(|(k,v)| (k.to_string(), v.to_string())).collect();
+        Ok(out_map)
+    }
+    fn render(&self, i : HashMap<String, String>) -> String {
+        let error_text = "ERROR!".to_string();
+        let quote = i.get(&"quote".to_string()).unwrap_or(&error_text);
+        format!("{}", quote)
+    }
+}
+
+
+
+#[derive(Clone)]
+pub struct Battery;
+
+#[async_trait]
+impl Handler for Battery {
+    async fn handle(&self) -> StdResult<HashMap<String,String>, Box<dyn Error>> {
+        let bat_path = "/sys/class/power_supply/BAT0";
+        let cap_path = format!("{}/capacity", bat_path);
+        let stat_path = format!("{}/status", bat_path);
+        let cap_string = std::fs::read_to_string(cap_path)?.trim().replace("\"", "");
+        let stat_string = std::fs::read_to_string(stat_path)?.trim().to_lowercase().replace(" ", "");
+
+        let mut out_map = HashMap::new(); 
+        out_map.insert("capacity".to_string(), cap_string.to_string());
+        out_map.insert("status".to_string(), stat_string.to_string());
+        Ok(out_map) 
+    }
+
+    fn render(&self, i : HashMap<String, String>) -> String {
+        let empty = "".to_string();
+        let cap = i.get("capacity").unwrap_or(&empty);
+        let stat = i.get("status").unwrap_or(&empty).as_str();
+        format!("{} {}%", bat_status_icons(stat) , cap)
+    }
+}
+
+
+
+#[derive(Clone)]
+pub struct Wifi;
+
+#[async_trait]
+impl Handler for Wifi {
+    async fn handle(&self) -> StdResult<HashMap<String,String>, Box<dyn Error>> {
+
+        let wifi_cmd = Command::new("iw").arg("dev").output().await?;
+        let s : Vec<String> = String::from_utf8_lossy(&wifi_cmd.stdout).lines().map(|s| s.trim().to_string()).collect();
+        let interface : &str = s[5].split(" ").last().unwrap_or("");
+
+        let connected_cmd = Command::new("iw").arg(interface).arg("link").output().await?;
+        let s2 = String::from_utf8_lossy(&connected_cmd.stdout).find("Connected");
+
+        let is_connected = match s2 {
+            Some(_) => true,
+            None => false 
+        };
+
+        let connect_status = if is_connected {"connected"} else {"disconnected"};
+
+        let out_map : HashMap<String, String>  = 
+            [("connect_status", connect_status)]
+            .iter()
+                .map(|(k,v)| (k.to_string(), v.to_string()))
+                .collect();
+        //out_map.insert("connect_status".to_string(), connect_status.to_string());
+
+        Ok(out_map)
+    }
+
+    fn render(&self, i : HashMap<String, String>) -> String {
+
+        let EMPTY = "".to_string();
+        let connected = i.get("connect_status").unwrap_or(&EMPTY);
+        format!("{}", wifi_status_icons(&connected))
+    }
+}
+
+fn bat_status_icons(n: &str) -> &'static str {
+   match n {
+       "full" => "🟢",
+       "charging" => "⚡",
+       "notcharging" => "🔌",
+       "discharging" => "🔋",
+       _ => ""
+   }
+}
+
+
+#[derive(Clone)]
+pub struct Volume;
+#[async_trait]
+impl Handler for Volume {
+    async fn handle(&self) -> StdResult<HashMap<String,String>, Box<dyn Error>> {
+        let SPACE = " ".to_string();
+        let is_muted_cmd = Command::new("pactl").arg("get-sink-mute").arg("@DEFAULT_SINK@").output().await?;
+        let vol_info_cmd = Command::new("pactl").arg("get-sink-volume").arg("@DEFAULT_SINK@").output().await?; 
+        let vol_info_str = String::from_utf8_lossy(&vol_info_cmd.stdout);
+        let vol_info : Vec<String> = vol_info_str.split(&SPACE).map(|i| i.to_string()).collect();
+        let is_muted_raw_str = String::from_utf8_lossy(&is_muted_cmd.stdout);
+        let is_muted_lower = is_muted_raw_str.to_lowercase();
+        let is_muted_str = is_muted_lower.split(&SPACE).map(|x| x.trim()).last(); 
+
+        let is_muted = match is_muted_str {
+            Some("yes") => "muted",
+            _ => "not muted" 
+        }.to_string();
+
+        let vol_level = if vol_info[5] == "/" {vol_info[4].to_string()} else {vol_info[5].to_string()}.replace("%","");
+        let out_map : HashMap<String, String> = [("volume_level", vol_level), ("is_muted", is_muted)].iter().map(|(k,v)| (k.to_string(), v.to_string())).collect(); 
+
+        Ok(out_map)
+    }
+
+    fn render(&self, i : HashMap<String, String>) -> String {
+        let default_muted = "default_muted".to_string();
+        let is_muted = i.get("is_muted").unwrap_or(&default_muted);
+        let small_speaker_cutoff = 40;
+        let mid_speaker_cutoff = 80; 
+        let default_vol = "50".to_string();
+        let vol_level_str = i.get("volume_level").unwrap_or(&default_vol); 
+
+        let vol_level : i32 = vol_level_str.parse().unwrap_or(50); 
+
+        let icon = if is_muted == "muted" {
+            "🔇"
+        } else if vol_level < small_speaker_cutoff {
+            "🔈"
+        } else if vol_level < mid_speaker_cutoff {
+            "🔉"
+        } else {
+            "🔊"
+        };
+
+        format!("{}{}%", icon, vol_level_str)
+
+    }
+}
+
+
+#[derive(Clone)]
+pub struct Date;
+
+#[async_trait]
+impl Handler for Date{
+    async fn handle(&self) -> StdResult<HashMap<String,String>, Box<dyn Error>> {
+        let now = Local::now();
+        let weekday = now.weekday();
+        let day = now.day();
+        let month = month_abbr(now.month());
+        let seconds = format!("{:02}", now.second());
+        let mod_hour = if now.hour() % 12 == 0 {12} else {now.hour() % 12};
+ 
+        let hour = format!("{:02}", mod_hour);  
+        let minutes = format!("{:02}", now.minute());
+        let mut out_hash = HashMap::new();
+        out_hash.insert("weekday".to_string(), weekday.to_string());
+        out_hash.insert("day".to_string(), day.to_string());
+        out_hash.insert("month".to_string(), month.to_string());
+        out_hash.insert("seconds".to_string(), seconds.to_string());
+        out_hash.insert("minutes".to_string(), minutes.to_string());
+        out_hash.insert("hour".to_string(), hour.to_string());
+        Ok(out_hash)
+    }
+
+    fn render(&self, i : HashMap<String, String>) -> String {
+        static EMPTY: String = String::new();
+
+        let hour = i.get("hour").unwrap_or(&EMPTY);
+        let minute = i.get("minutes").unwrap_or(&EMPTY);
+        let day = i.get("day").unwrap_or(&EMPTY);
+        let month = i.get("month").unwrap_or(&EMPTY);
+        let weekday = i.get("weekday").unwrap_or(&EMPTY);
+        let seconds = i.get("seconds").unwrap_or(&EMPTY);
+        format!("{} {} {} {}:{} {}", weekday, month, day, hour, minute, seconds )
+    }
+}
+
+
+#[derive(Serialize, Deserialize)]
+pub struct Out {
+   pub name : String, 
+   pub instance: String,
+   pub full_text: String
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct PersistConfig  {
+    pub path: String,
+    pub buffer_size : i32
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct ModuleConfig {
+   pub name: String,
+   pub ttl: u64
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Config {
+   pub poll_time : u64,
+   pub default_timeout: i32, 
+   pub persist : PersistConfig, 
+   pub modules : Vec<ModuleConfig>
+}
